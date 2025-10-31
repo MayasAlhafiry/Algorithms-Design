3bd93612d130cea5b56e0b7b6508eff78479e907
Rana-Hamdi <ranahamdiafifi@gmail.com>
2025-10-31 18:27:23 +0200

    make the dynamic array into vector,error handling,its done

--- Task 1.cpp
+++ Task 1.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 // Implement the Fibonacci series using recursion
 int fibonacci_recursion(int n) {
-    if (n <= 1)
+	if (n<0){
+	  cout << "n is a position ,please enter a valid position" << endl;
+        return -1;
+	}
+    if (n==0 || n==1)
         return n; // base cases: fib(0)=0, fib(1)=1
     return fibonacci_recursion(n - 1) + fibonacci_recursion(n - 2); // recursive case
 }
@@ -21,6 +25,10 @@ void multiply_fib_matrix(int arr[2][2], int arr2[2][2], int res[2][2]) {
 }
 
 void powerMatrix(int mat[2][2], int n, int powerRes[2][2]) {
+	if (n<0){
+	cout << "n is a position ,please enter a valid position" << endl;
+	  return;
+	}
     if (n == 1) {
         powerRes[0][0] = mat[0][0];
         powerRes[0][1] = mat[0][1];
@@ -64,27 +72,31 @@ int fibonacci_DC(int n) {
 
 // this is Fibonacci series using dynamic programming.
 long fib(int n) {
-    // where n is the position and u get the value
-    if (n < 0) {
-        cout << "Error in Fibonacci number" << endl;
+    //  n position in Fibonacci sequence
+    if (n < 0) { // Check that the input givin isnt negative
+        cout << "n is a position ,please enter a valid position" << endl;
+        return -1;
     }
-    if (n == 0 || n == 1) {
+    // if n is poisition 0 or 1 return n beacuse they are the same as their input in the sequence 
+    if (n == 0 || n == 1) { 
         return n;
     }
-
-    long fibArray[n + 1];
-    fibArray[0] = 0;
-    fibArray[1] = 1;
-    for (int i = 2; i <= n; i++) {
+    // an array/vector to store Fibonacci numbers from position 0 to n
+    vector<long> fibArray(n + 1);
+      fibArray[0] = 0;
+      fibArray[1] = 1;
+    for (int i = 2; i <= n; i++) { // for loop to get the answer as for every loop use the before answers to get it
         fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
-    }
-    return fibArray[n];
+    } 
+    return fibArray[n]; // return result 
 }
 
 
 
 int main() {
-    int n = 10;
+    cout<<"enter n to get Fibonacci for it using the three methods :"<<endl;
+    int n ;
+    cin>>n;
     cout << "Fibonacci of " << n << " using Recursion: " << fibonacci_recursion(n) << endl;
     cout << "Fibonacci of " << n << " using Divide and Conquer: " << fibonacci_DC(n) << endl;
     cout << "Fibonacci of " << n << " using Dynamic Programming: " << fib(n) << endl;
